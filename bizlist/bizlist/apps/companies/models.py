1053dc0e85a7c5cca3b6a753ce650726895383ba
Dedi Purwanto <kecebongsoft@gmail.com>
2013-01-28 16:12:45 +0800

    Added models and registered to settings

---
+++ bizlist/bizlist/apps/companies/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+from django.template.defaultfilters import slugify
+
+from references.models import Location, Category
+
+class Company(models.Model):
+
+    title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, blank=True, null=True)
+    featured = models.BooleanField(default=False)
+    description = models.TextField()
+    meta_keywords = models.CharField(max_length=255, blank=True, null=True)
+    meta_description = models.TextField(blank=True, null=True)
+    categories = models.ManyToManyField(Category)
+    location = models.ForeignKey(Location)
+    address = models.TextField(blank=True, null=True)
+    telephone = models.CharField(max_length=255, blank=True, null=True)
+    fax = models.CharField(max_length=255, blank=True, null=True)
+    website = models.URLField(blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    person_in_charge = models.CharField(max_length=255, blank=True, null=True)
+    person_position = models.CharField(max_length=255, blank=True, null=True)
+    description_wanted = models.TextField(blank=True, null=True)
+    logo_url = models.URLField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Company, self).save(*args, **kwargs)
+
+class Product(models.Model):
+    title = models.CharField(max_length=255)
+    slug = models.CharField(max_length=255, blank=True, null=True)
+    meta_keywords = models.CharField(max_length=255, blank=True, null=True)
+    meta_description = models.TextField(blank=True, null=True)
+    description = models.TextField()
+    company = models.ForeignKey(Company)
+    category = models.ForeignKey(Category)
+    price = models.CharField(max_length=255)
+    price_remakrs = models.CharField(max_length=255, blank=True, null=True)
+    photo_url = models.URLField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Company, self).save(*args, **kwargs)
+
+
+class Photo(models.Model):
+    picture = models.ImageField('Picture', blank=True, null=True, upload_to='photos/%Y/%m/%d')
+    company = models.ForeignKey(Company, blank=True, null=True)
--- bizlist/bizlist/settings.py
+++ bizlist/bizlist/settings.py
@@ -137,6 +137,7 @@ INSTALLED_APPS = (
 
     'references',
     'articles',
+    'companies'
 )
 
 # A sample logging configuration. The only tangible logging
