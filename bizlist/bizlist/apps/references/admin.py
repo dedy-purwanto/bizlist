8cc617ceeed9a85bbf29f8f597d4d584ae391736
Dedi Purwanto <kecebongsoft@gmail.com>
2013-01-28 15:50:59 +0800

    Added category into admin

--- bizlist/bizlist/apps/references/admin.py
+++ bizlist/bizlist/apps/references/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import State
+from .models import State, Category
 
 
 class StateAdmin(admin.ModelAdmin):
@@ -8,6 +8,21 @@ class StateAdmin(admin.ModelAdmin):
     list_display = (
             'id',
             'title',
+            'slug',
     )
 
 admin.site.register(State, StateAdmin)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+            'id',
+            'title',
+            'slug',
+            'meta_keywords',
+            'meta_description',
+            'parent',
+    )
+
+admin.site.register(Category, CategoryAdmin)
