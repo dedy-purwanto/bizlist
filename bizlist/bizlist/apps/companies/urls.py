c84dd4721462a2f77ab02a33c7add23e31ca58fc
Dedi Purwanto <kecebongsoft@gmail.com>
2013-02-02 23:07:00 +0800

    Updated company and product detail url

--- bizlist/bizlist/apps/companies/urls.py
+++ bizlist/bizlist/apps/companies/urls.py
@@ -5,7 +5,7 @@ from .views import CompanyListView, CompanyDetailView, ProductListView, ProductD
 urlpatterns = patterns('companies.views',
     url(r'^myanmar-burma-companies/(?P<state>[-\w]+)/(?P<category>[-\w]+)/(?P<sub_category>[-\w]+)/$', CompanyListView.as_view(), name='company_list'),
     url(r'^myanmar-burma-products/(?P<state>[-\w]+)/(?P<category>[-\w]+)/(?P<sub_category>[-\w]+)/$', ProductListView.as_view(), name='product_list'),
-    url(r'^(?P<company_slug>[-\w]+)/$', CompanyDetailView.as_view(), name='company_detail'),
-    url(r'^(?P<company_slug>[-\w]+)/products/(?P<product_slug>[-\w]+)/$', ProductDetailView.as_view(), name='product_detail'),
+    url(r'^company/(?P<company_pk>\d+)/(?P<company_slug>[-\w]+)/$', CompanyDetailView.as_view(), name='company_detail'),
+    url(r'^company/(?P<company_pk>\d+)/(?P<company_slug>[-\w]+)/product/(?P<product_pk>\d+)/(?P<product_slug>[-\w]+)/$', ProductDetailView.as_view(), name='product_detail'),
 
 )
