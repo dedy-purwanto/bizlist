b4fc80368de79cc9bc64a7b196aafc082db05568
Dedi Purwanto <kecebongsoft@gmail.com>
2013-02-03 00:35:20 +0800

    Show list of categories in company and product list when no criteria is selected Fix category parent product and category counter

--- bizlist/bizlist/apps/companies/views.py
+++ bizlist/bizlist/apps/companies/views.py
@@ -42,6 +42,22 @@ class ListViewMixin(object):
         if category is not None:
             context['sub_categories'] = Category.objects.filter(parent=category).order_by('title')
 
+        
+        context['states'] = State.objects.all().order_by('title')
+        categories = Category.objects.filter(parent=None)
+        categories_column = []
+        categories_rows = 8
+
+        for i in range(0,99):
+            categories_column.append(categories[i * categories_rows : (i * categories_rows) + categories_rows])
+            if i * categories_rows > categories.count() - 1:
+                break
+
+        if len(categories_column[len(categories_column)-1]) == 0:
+            categories_column.pop()
+
+        context['categories_column'] = categories_column
+
         return context
 
 
--- bizlist/bizlist/apps/references/models.py
+++ bizlist/bizlist/apps/references/models.py
@@ -1,10 +1,18 @@
 from django.db import models
+from django.db.models import Q
 from django.template.defaultfilters import slugify
+
 
 class State(models.Model):
 
     title = models.CharField(max_length=255)
     slug = models.CharField(max_length=255, blank=True, null=True)
+
+    def get_products(self):
+        from companies.models import Product
+        products = Product.objects.filter(company__state=self).distinct()
+        return products
+        
 
     def __unicode__(self):
         return "%s" % self.title
@@ -21,6 +29,24 @@ class Category(models.Model):
     meta_keywords = models.CharField(max_length=255, blank=True, null=True)
     meta_description = models.TextField(blank=True, null=True)
     parent = models.ForeignKey('self', blank=True, null=True)
+
+    # Called from parent category, fetch list of company attached 
+    # to this category along with its subcategory
+    def get_companies(self):
+        from companies.models import Company
+        companies = Company.objects.filter(
+                Q(categories=self) |
+                Q(categories__parent=self)
+                ).distinct()
+        return companies
+
+    def get_products(self):
+        from companies.models import Product
+        products = Product.objects.filter(
+                Q(category=self) |
+                Q(category__parent=self)
+                ).distinct()
+        return products
 
     def __unicode__(self):
         return "%s" % self.title
